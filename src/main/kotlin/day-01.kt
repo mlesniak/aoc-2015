973f5b954513bd8711b7612c09d299634748e26f
Michael Lesniak <mail@mlesniak.com>
2021-05-15 19:45:04 +0200

    [01] Part 2

--- src/main/kotlin/day-01.kt
+++ src/main/kotlin/day-01.kt
@@ -3,9 +3,21 @@ import java.nio.file.Path
 
 fun main() {
     val input = Files.readString(Path.of("input/01.txt"))
-    val counts = input
-        .groupBy { it }
-        .map { it.value.size}
-    val result = counts[0] - counts[1]
+
+    // val counts = input
+    //     .groupBy { it }
+    //     .map { it.value.size}
+    // val result = counts[0] - counts[1]
+    // println(result)
+
+    var floor = 0
+    var result = 0
+    for (index in input.indices) {
+        floor += if (input[index] == '(') 1 else -1
+        if (floor == -1) {
+            result = index+1
+            break
+        }
+    }
     println(result)
 }
