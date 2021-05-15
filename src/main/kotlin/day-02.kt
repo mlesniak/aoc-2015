e1b01f0b5ed005c89c7087360251b22c8e0811df
Michael Lesniak <mail@mlesniak.com>
2021-05-15 20:21:39 +0200

    [02] Part 2

--- src/main/kotlin/day-02.kt
+++ src/main/kotlin/day-02.kt
@@ -8,8 +8,19 @@ data class Present(val length: Int, val width: Int, val height: Int) {
             length * height,
             width * height
         )
-        val smallestSide = areaOfSides.minOrNull()!!
-        return 2 * length * width + 2 * width * height + 2 * height * length + smallestSide
+        val smallestSide = areaOfSides.minOrNull()
+        return 2 * length * width + 2 * width * height + 2 * height * length +
+            (smallestSide ?: throw IllegalStateException("Minimum side not found"))
+    }
+
+    fun shortestPerimeter(): Int {
+        val perimeters = listOf(
+            2 * length + 2 * width,
+            2 * length + 2 * height,
+            2 * width + 2 * height
+        )
+        val smallestPerimeter = perimeters.minOrNull()
+        return smallestPerimeter ?: throw IllegalStateException("Minimum perimeter not found")
     }
 }
 
@@ -20,7 +31,14 @@ fun main() {
         Present(parts[0], parts[1], parts[2])
     }
 
-    val paperLengths = presents.map { it.computeWrappingPaperLength() }
-    val result = paperLengths.sum()
+    // val paperLengths = presents.map { it.computeWrappingPaperLength() }
+    // val result = paperLengths.sum()
+    // println(result)
+
+    val result = presents.sumOf {
+        val ribbon = it.shortestPerimeter()
+        val bow = it.width * it.height * it.length
+        ribbon + bow
+    }
     println(result)
 }
